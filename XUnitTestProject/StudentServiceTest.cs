66dbd665345b65a9aa4d032e0335025efebeb113
BentHPedersen <benthpedersen@mail.com>
2022-09-21 10:50:26 +0200

    Some comments added

--- XUnitTestProject/StudentServiceTest.cs
+++ XUnitTestProject/StudentServiceTest.cs
@@ -25,21 +25,30 @@ namespace XUnitTestProject
         public void AddValidStudent()
         {   
             // Arrange
+
+            // List keeping the data of the mocked repository
             List<Student> data = new List<Student>();
+
+            // The mock of the repository
             Mock<IStudentRepository> mockRepository = new Mock<IStudentRepository>();
+            
+            // Setup of the Add method of the Mock to work with the data list.
             mockRepository.Setup(r => r.Add(It.IsAny<Student>())).Callback<Student>(s => data.Add(s));
             
+            // StudentService with injected mockRepository (remember to use .Object)
             StudentService service = new StudentService(mockRepository.Object);
 
+            // the student to add
             Student student = new Student(1, "Name", "Email");
 
             // Act
             service.AddStudent(student);
 
             // Assert
-            Assert.True(data.Count == 1);
-            Assert.Equal(student, data[0]);
-            mockRepository.Verify(r => r.Add(student), Times.Once);
+            Assert.True(data.Count == 1); // one student inserted into the data list
+            Assert.Equal(student, data[0]); // and it is indeed the right student !
+            mockRepository.Verify(r => r.Add(student), Times.Once); // and the service is using the repository.Add
+
         }
     }
 }
